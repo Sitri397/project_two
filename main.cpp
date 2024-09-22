15bd1a0e14922904ba2bea61fd677e889f8be5dd
Sitri397 <nikita.demin707@gmail.com>
2024-09-22 12:25:03 +0300

    Update main.cpp
    Pagal aprašytus užduoties reikalavimus realizuokite programą, kuri nuskaito vartotojų įvedamus reikiamus duomenis (patogiausiai naudoti struktūrą):
    string tipo: studento vardą ir pavardę;
    int tipo: namų darbų tarpiniai rezultatai ir egzamino rezultatą;

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-int main(){
-  cout << "Hello World!" << endl;
-  return 0;
+struct Student{
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egzaminas;
+};
+
+void ived(Student &st){
+    cout << "Iveskite studento varda, pavarde ir egzamino rezultata: " << endl;
+    cin >> st.vardas >> st.pavarde >> st.egzaminas;
+    cout << "Iveskite studento namu darbu skaiciu: " << endl;
+    int n;
+    int k;
+    cin >> n;
+    cout << "Iveskite namu darbu rezultatus: " << endl;
+    for(int i = 0;i < n;i++){
+      cin >> k;
+      st.nd.push_back(k);
+    }
+
 }
-  
+
+void valymas(Student &Lok){
+    Lok.vardas.clear();
+    Lok.pavarde.clear();
+    Lok.nd.clear();
+}
+
+int main(){
+    vector<Student> Vec1;
+    Student Temp;
+    cout << "Kiek studentu?: " << endl;
+    int n;
+    cin >> n;
+    for(int i = 0;i < n;i++){
+        cout << "Iveskite studento duomenis: " << endl;
+        ived(Temp);
+        Vec1.push_back(Temp);
+        valymas(Temp);
+    }
+    return 0;
+}
+
