0fd78bc3ce949e4a2f5fd5d8d063c496791ec6f6
Sitri397 <nikita.demin707@gmail.com>
2024-11-08 18:23:29 +0200

    Add files via upload

---
+++ Include/mylib.h
@@ -0,0 +1,53 @@
+//mylib.h
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <random>
+#include <stdexcept>
+#include <sstream>
+#include <chrono>
+#include <list>
+#include <iterator>
+
+using std::list;
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::istringstream;
+using std::runtime_error;
+using std::srand;
+using std::rand;
+using std::time;
+using std::sort;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::left;
+using std::right;
+using std::getline;
+using std::exception;
+using std::invalid_argument;
+using std::ofstream;
+using std::stringstream;
+using std::ostringstream;
+using std::cerr;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::to_string;
+using std::remove;
+using std::swap;
+
+
+
+
+#endif // MYLIB_H_INCLUDED
