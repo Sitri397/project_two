ca495afb0833bf4570f505e91df0fca35ef35274
Sitri397 <nikita.demin707@gmail.com>
2024-11-20 16:33:14 +0200

    Add files via upload

--- src/stud.cpp
+++ src/stud.cpp
@@ -1,125 +1,50 @@
-//stud.cpp
 #include "Student.h"
 
-
-void ived(Student &st) {
-    string input;
-    int rezultatas;
+void ived(Student& st) {
+    string vardas, pavarde;
     char pasirinkimas;
-    cout << "Iveskite studento varda ir pavarde: " << endl;
-    cin >> st.vardas >> st.pavarde;
 
-    cout << "Ar sugeneruoti studento namu darbu ir egzamino rezultatus? (t/n): " << endl;
+    cout << "Iveskite studento varda ir pavarde: ";
+    cin >> vardas >> pavarde;
+    st.setVardas(vardas);
+    st.setPavarde(pavarde);
+
+    cout << "Ar sugeneruoti studento rezultatus? (t/n): ";
     cin >> pasirinkimas;
+
     if (pasirinkimas == 't') {
         srand(time(nullptr));
         for (int i = 0; i < 5; ++i) {
-            st.nd.push_back(rand() % 10 + 1);
+            st.addNd(rand() % 10 + 1);
         }
-        st.egzaminas = rand() % 10 + 1;
-    } else if (pasirinkimas == 'n') {
-        cout << "Iveskite namu darbu rezultatus (-1 uzbaigti)" << endl;
-        while (true) {
-            cin >> input;
-            try {
-                rezultatas = stoi(input);
-                if (rezultatas == -1) break;
-                if (rezultatas < 0 || rezultatas > 10) {
-                    cout << "Netinkamas ivestis! Prasome ivesti skaiciu nuo 1 iki 10 arba -1, kad uzbaigti." << endl;
-                    continue;
-                }
-                st.nd.push_back(rezultatas);
-            } catch (invalid_argument&) {
-                cout << "Neteisingas formatas! Prasome ivesti skaiciu arba -1, kad baigti." << endl;
-                cin.clear();
-                cin.ignore();
-            }
-        }
-
-
-        cout << "Iveskite egzamino rezultata: " << endl;
-        while (true) {
-            cin >> input;
-            try {
-                st.egzaminas = stoi(input);
-                if (st.egzaminas < 0 || st.egzaminas > 10) {
-                    cout << "Netinkamas egzamino rezultatas! Prasome ivesti skaiciu nuo 1 iki 10." << endl;
-                    continue;
-                }
-                break;
-            } catch (invalid_argument&) {
-                cout << "Neteisingas formatas! Prasome ivesti skaiciu." << endl;
-                cin.clear();
-                cin.ignore();
-            }
-        }
-    }
-}
-
-
-void valymas(Student &Lok) {
-    Lok.vardas.clear();
-    Lok.pavarde.clear();
-    Lok.nd.clear();
-}
-
-
-double Gal_Balas_vid(Student &Lok) {
-    if (Lok.nd.empty()) throw runtime_error("Nera ivestu namu darbu rezultatu");
-    double sum = 0;
-    for (int i = 0; i < Lok.nd.size(); i++) {
-        sum += Lok.nd.at(i);
-    }
-    double vid = sum / Lok.nd.size();
-    return 0.4 * vid + Lok.egzaminas * 0.6;
-}
-
-
-double Gal_Balas_med(Student &Lok) {
-    if (Lok.nd.empty()) throw runtime_error("Nera ivestu namu darbu rezultatu");
-    vector<int> darbai = Lok.nd;
-    sort(darbai.begin(), darbai.end());
-    double mediana;
-    int size = darbai.size();
-
-    if (size % 2 == 0) {
-        mediana = (darbai[size / 2 - 1] + darbai[size / 2]) / 2.0;
+        st.setEgzaminas(rand() % 10 + 1);
     } else {
-        mediana = darbai[size / 2];
-    }
-    return 0.4 * mediana + 0.6 * Lok.egzaminas;
-}
-
-
-
-
-
-int comper_by_name(Student& st1, Student& st2) {
-    if (st1.vardas != st2.vardas) {
-        return st1.vardas < st2.vardas;
-    } else {
-        return st1.pavarde < st2.pavarde;
+        int grade;
+        cout << "Iveskite namu darbu rezultatus (-1 uzbaigti): ";
+        while (cin >> grade && grade != -1) {
+            st.addNd(grade);
+        }
+        cout << "Iveskite egzamino rezultata: ";
+        cin >> grade;
+        st.setEgzaminas(grade);
     }
 }
 
-int comper_by_last_name(Student& st1, Student& st2) {
-    if (st1.pavarde != st2.pavarde){
-        return st1.pavarde < st2.pavarde;
-    }
-    else{
-        return st1.pavarde > st2.pavarde;
-    }
+double Student::calcGalBalasVid() const {
+    if (nd_.empty()) throw runtime_error("Nera ivestu namu darbu rezultatu");
+    double vidurkis = accumulate(nd_.begin(), nd_.end(), 0.0) / nd_.size();
+    return 0.4 * vidurkis + 0.6 * egzaminas_;
 }
 
-int comper_by_mark(Student& st1, Student& st2) {
-    if (st1.galutinis != st2.galutinis){
-        return st1.galutinis < st2.galutinis;
-    }
-    else{
-        return st1.galutinis > st2.galutinis;
-    }
+double Student::calcGalBalasMed() const {
+    if (nd_.empty()) throw runtime_error("Nera ivestu namu darbu rezultatu");
+    vector<int> sortedNd = nd_;
+    sort(sortedNd.begin(), sortedNd.end());
+    double mediana = (sortedNd.size() % 2 == 0)
+                     ? (sortedNd[sortedNd.size() / 2 - 1] + sortedNd[sortedNd.size() / 2]) / 2.0
+                     : sortedNd[sortedNd.size() / 2];
+    return 0.4 * mediana + 0.6 * egzaminas_;
 }
-
 
 int capacity_nustatymas(string failo_pavadinimas){
     int cap = 0;
@@ -175,6 +100,13 @@ void generuotiFaila(const string& failoPavadinimas, int studentuSk) {
     outFile.close();
 }
 
-
-
+bool compareByName(const Student& st1, const Student& st2) {
+    return st1.get_vardas() < st2.get_vardas()|| (st1.get_vardas() == st2.get_vardas() && st1.get_pavarde() < st2.get_pavarde());
+}
+bool compareByLastName(const Student& st1, const Student& st2) {
+    return st1.get_pavarde() < st2.get_pavarde();
+}
+bool compareByMark(const Student& st1, const Student& st2) {
+    return st1.get_galutinis() < st2.get_galutinis();
+}
 
